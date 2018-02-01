7d48e118bc88bf67c13e49cfc359627dcb40318b
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-01 17:26:38 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Main(object):
+
+    def __init__(self):
+        self.do_something()
+
+    def do_something(self):
+        print "hello, World!"
+
+
+if __name__ == "__main__":
+    Main()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='Sample Code',
+      version='1.0',
+      description='Python Sample Code'
+      )
