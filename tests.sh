f104eef55cf7373aa1074b744bf23cb876a2b996
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-20 17:14:35 +0000

    updates

--- tests.sh
+++ tests.sh
@@ -3,13 +3,15 @@
 # exit on first non-zero exit code
 set -ex -o pipefail
 
-
+# create directory
 mkdir -p reports
 
+# run tests
 python -m nose tests/*.py \
     --with-xunit --xunit-file=reports/junit.min.xml \
     --with-coverage --cover-branches --cover-inclusive --cover-tests \
     --cover-xml --cover-xml-file=reports/coverage.xml \
     --cover-html --cover-html-dir=reports/coverage
 
+# convert nosetest xml from mini to expanded
 xmllint --format reports/junit.min.xml > reports/junit.xml
