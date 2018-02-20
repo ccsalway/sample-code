84fe3e6f4d764c1695b31e0850a07b58df713b4d
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-20 15:07:04 +0000

    updates

--- tests.sh
+++ tests.sh
@@ -2,8 +2,4 @@
 
 mkdir -p reports
 
-python -m nose tests/*.py --with-xunit --with-coverage --cover-branches --cover-inclusive --cover-tests --xunit-file=reports/nosetests.xml
-
-python -m coverage xml
-
-mv coverage.xml reports/coverage.xml
+python -m nose tests/*.py --with-xunit --xunit-file=reports/nosetests.xml --with-coverage --cover-branches --cover-inclusive --cover-tests --cover-xml --cover-xml-file=reports/coverage.xml
