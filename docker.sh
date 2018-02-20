5e437ac383bbe35d802e077c9630ff5256f7296e
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-20 14:15:03 +0000

    updates

--- docker.sh
+++ docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Pipeline: ${GO_PIPELINE_NAME}"
+echo "Building: ${GO_PIPELINE_NAME}"
 
 # build image
 docker build . -t app/sample-code
 
 # run tests
-docker run --rm -v /tmp/reports:/usr/src/app/reports --entrypoint python app/sample-code -m nose --with-xunit --xunit-file=reports/nosetests.xml
+docker run --rm -v /godata/pipelines/${GO_PIPELINE_NAME}/reports:/usr/src/app/reports --entrypoint python app/sample-code -m nose --with-xunit --xunit-file=reports/nosetests.xml
 
 # run app
 docker run --rm -i --entrypoint python app/sample-code entry.py
