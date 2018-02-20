77bbdf23a029db7c5e2d4a0ca1c0106e410aa504
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-20 14:33:46 +0000

    updates

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ echo "Building: ${GO_PIPELINE_NAME}"
 docker build . -t app/sample-code
 
 # run tests
-docker run --rm -v /godata/pipelines/${GO_PIPELINE_NAME}/reports:/usr/src/app/reports --entrypoint python app/sample-code -m nose --with-xunit --xunit-file=reports/nosetests.xml
+docker run --rm -v /tmp/godata/pipelines/${GO_PIPELINE_NAME}/reports:/usr/src/app/reports --entrypoint python app/sample-code -m nose --with-xunit --xunit-file=reports/nosetests.xml
 
 # run app
 docker run --rm -i --entrypoint python app/sample-code entry.py
--- gocd-agent/docker.sh
+++ gocd-agent/docker.sh
@@ -6,6 +6,7 @@ docker build . -t gocd-agent-docker
 # run image
 docker run -d \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /tmp/godata:/godata \
     -e GO_SERVER_URL=https://$(docker inspect --format='{{(index (index .NetworkSettings.IPAddress))}}' gocd_server):8154/go \
     -e AGENT_AUTO_REGISTER_KEY=4c635d73-9cbe-4ecc-a68c-4b1fbb6eeca0 \
     gocd-agent-docker
