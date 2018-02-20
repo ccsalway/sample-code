4c580f1c598e752675e060fb695e9a54e098bf71
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-20 15:32:43 +0000

    updates

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 # build image
 docker build . -t app/sample-code
 
-# run tests - just remove /tmp when running the Agent as its own EC2
+# run tests - just remove /tmp when running the Agent on its own EC2
 docker run --rm \
     -v /tmp/godata/pipelines/${GO_PIPELINE_NAME}/reports:/usr/src/app/reports \
     --entrypoint sh app/sample-code tests.sh
--- tests.sh
+++ tests.sh
@@ -2,4 +2,8 @@
 
 mkdir -p reports
 
-python -m nose tests/*.py --with-xunit --xunit-file=reports/nosetests.xml --with-coverage --cover-branches --cover-inclusive --cover-tests --cover-xml --cover-xml-file=reports/coverage.xml
+python -m nose tests/*.py \
+    --with-xunit --xunit-file=reports/nosetests.xml \
+    --with-coverage --cover-branches --cover-inclusive --cover-tests \
+    --cover-xml --cover-xml-file=reports/coverage.xml \
+    --cover-html --cover-html-dir=reports/html
