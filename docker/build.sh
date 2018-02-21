ef9a16aadf14a0ba601c0b12bd07af9522b5e52c
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-21 16:08:04 +0000

    updates

--- docker/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# build image
-docker build .. -t app/sample-code
--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# run app
-docker run --rm -i --entrypoint python app/sample-code entry.py
