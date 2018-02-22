28ce0677a2b466fcb06abf3cf622a6922eebfe3d
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-22 14:49:13 +0000

    updates

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@
 echo "Current Working Directory: ${PWD}"
 ls -l
 
+###### BITBUCKET AUTH TOKEN
+
 # request access token from BitBucket (valid for 1hr)
 RESP=$(curl -s -X POST https://bitbucket.org/site/oauth2/access_token \
     -d grant_type=client_credentials \
@@ -15,4 +17,10 @@ RESP=$(curl -s -X POST https://bitbucket.org/site/oauth2/access_token \
 TOKEN=$(python -c "import sys, json; print json.loads(sys.argv[1])['access_token']" "${RESP}")
 
 # replace TOKEN in setup.py
-sed -i "" "s|AUTH_TOKEN|${TOKEN}|" ./setup.py
+sed -i "s|auth = ''|auth = 'x-token-auth:${TOKEN}@'|" ./setup.py
+
+
+########## BUILD
+
+# build docker
+docker build . -t ${DOCKER_TAG}
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
-scm = "https://x-token-auth:AUTH_TOKEN=@bitbucket.org/naimuri/"
+auth = ''
+scm = "git+https://%sbitbucket.org/naimuri/" % auth
 
 setup(
     name='sample-code',
     version='1.0',
+    description='Sample Code for learning',
+    author='Christian Salway',
     packages=find_packages(),
-    install_requires=['nose', 'coverage', 'pylint'],
+    install_requires=['nose', 'coverage', 'pylint', 'cyrm_common', 'cyrm_python_tools_framework'],
     dependency_links=[
-        scm + "cyrm_common.git/master#egg=cyrm_common",
-        scm + "cyrm_python_tools_framework.git/master#egg=cyrm_python_tools_framework"
+        scm + "cyrm_common/master#egg=cyrm_common",
+        scm + "cyrm_python_tools_framework/master#egg=cyrm_python_tools_framework"
     ]
 )
