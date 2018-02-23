7febce956b5be14fd8a1b3ab59943dde61ab558d
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-23 10:13:27 +0000

    updates

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,8 +8,8 @@ class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEqual(main.main(), "Hello, World!")
 
-    #def test_fail(self):
-    #    self.assertFalse(True)
+    def test_fail(self):
+        self.assertFalse(True)
 
 
 if __name__ == '__main__':
